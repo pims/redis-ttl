c770e2df85b32d2c271f131ba0f06f6c7f89af19
Tim Bart <tim@pims.me>
2024-04-14 18:01:08 -0700

    fix tests

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -7,19 +7,29 @@ import (
 )
 
 func TestRun(t *testing.T) {
+
+	s := miniredis.RunT(t)
+
 	if err := run([]string{"redis-ttl"}); err == nil {
 		t.Fatal("expected error, got nil")
 	}
 
-	if err := run([]string{"redis-ttl", "--desired-ttl=1w"}); err != nil {
+	if err := run([]string{
+		"redis-ttl",
+		"--desired-ttl=1w",
+		"--redis-addr=" + s.Addr(),
+	}); err != nil {
 		t.Fatalf("expected nil, got: %v", err)
 	}
 
-	if err := run([]string{"redis-ttl", "--desired-ttl=0s"}); err == nil {
+	if err := run([]string{
+		"redis-ttl",
+		"--desired-ttl=0s",
+		"--redis-addr=" + s.Addr(),
+	}); err == nil {
 		t.Fatal("expected error got nil")
 	}
 
-	s := miniredis.RunT(t)
 	s.SetError("fault-injected")
 	if err := run([]string{
 		"redis-ttl",
